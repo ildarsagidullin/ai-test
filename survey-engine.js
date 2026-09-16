48b1477cdc74bb104c1f9f0efea6829b9dc178df
ildarsagidullin <ildar98989888@gmail.com>
2026-09-16 14:00:12 +0700

    Bust cached assets in survey-engine.js

--- survey-engine.js
+++ survey-engine.js
@@ -1,4 +1,4 @@
-import { levelDefinitions, surveyQuestions } from "./survey-config.js";
+import { levelDefinitions, surveyQuestions } from "./survey-config.js?v=3";
 
 export function optionValue(option) {
   return typeof option === "string" ? option : option.value ?? option.label;
